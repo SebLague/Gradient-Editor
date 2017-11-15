1ea4cf578f7a7b1dc277612c473cd2405c9e4e86
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2017-11-15 19:02:43 +0200

    Ep 01

---
+++ Episode 01/Assets/CustomGradient.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CustomGradient {
+
+
+    public Color Evaluate(float time)
+    {
+        return Color.Lerp(Color.white, Color.black, time);
+    }
+
+    public Texture2D GetTexture(int width)
+    {
+        Texture2D texture = new Texture2D(width, 1);
+        Color[] colours = new Color[width];
+        for (int i = 0; i < width; i++)
+        {
+            colours[i] = Evaluate((float)i / (width - 1));
+        }
+        texture.SetPixels(colours);
+        texture.Apply();
+        return texture;
+    }
+
+}
---
+++ Episode 01/Assets/Editor/GradientEditor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GradientEditor : EditorWindow {
+
+    private void OnEnable()
+    {
+        titleContent.text = "Gradient Editor";
+    }
+}
